be8fffeb452a2f4e41bb13a5fae5ff874bc2bd72
Nesa Nosrattalab <nesa.nosrattalab3@gmail.com>
2023-07-13 22:07:18 -0700

    validate gender

--- validation/validate_email.php
+++ validation/validate_email.php
@@ -9,14 +9,17 @@
                 $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
 
                 if (preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $sanitizedEmail)) {
-                    return true;
                     $validation_error['email'] = "";
+                    return true;
                 } else {
                     $validation_error['email'] = "Enter a valid email address.";
+                    return false;
                 }
             } else {
-                $validation_error['email'] = "Eamil is required.";
+                $validation_error['email'] = "email is required.";
+                return false;
             }
     }
    
 ?>
+
