d8a803ff19e683ba3e3e91b09a75b33309961551
Nesa Nosrattalab <nesa.nosrattalab3@gmail.com>
2023-07-13 23:54:01 -0700

    fixed identations

--- validation/validate_email.php
+++ validation/validate_email.php
@@ -1,29 +1,26 @@
-<?php 
-    $validation_error;
+<?php
+$validation_error;
 
-    function validate_email() {
-        global $validation_error;
+function validate_email()
+{
+    global $validation_error;
 
-            if (isset($_POST['email'])) {
-                $email = trim($_POST['email']);
-                $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+    if (isset($_POST['email'])) {
+        $email = trim($_POST['email']);
+        $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-                if (preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $sanitizedEmail)) {
+        if (preg_match('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $sanitizedEmail)) {
 
-                    $validation_error['email'] = "";
-                    return true;
+            $validation_error['email'] = "";
+            return true;
+        } else {
+            $validation_error['email'] = "Enter a valid email address.";
+            return false;
+        }
+    } else {
 
-                } else {
-                    $validation_error['email'] = "Enter a valid email address.";
-                    return false;
-                }
-            } else {
-                
-                $validation_error['email'] = "email is required.";
-                return false;
-
-            }
+        $validation_error['email'] = "email is required.";
+        return false;
     }
-   
+}
 ?>
-
--- validation/validate_gender.php
+++ validation/validate_gender.php
@@ -1,27 +1,27 @@
-<?php 
-    $validation_error;
+<?php
+$validation_error;
 
-    function validate_gender() {
-        global $validation_error;
+function validate_gender()
+{
+    global $validation_error;
 
-            if (isset($_POST['gender'])) {
-                $gender = $_POST['gender'];
-                $valid_genders = array('f', 'm', 'o');
+    if (isset($_POST['gender'])) {
+        $gender = $_POST['gender'];
+        $valid_genders = array('f', 'm', 'o');
 
-                $sanitizedGender = strtolower(trim($gender));
+        $sanitizedGender = strtolower(trim($gender));
 
-                if (in_array($sanitized_gender, $valid_genders)) {
-                    $validation_error['gender'] = "";
-                    return true;
-                } else {
-                    $validation_error['gender'] = "Enter a valid gender.";
-                    return false;
-                }
-
-            } else {
-                $validation_error['gender'] = "gender is required.";
-                return false;
-            }         
+        if (in_array($sanitizedGender, $valid_genders)) {
+            $validation_error['gender'] = "";
+            return true;
+        } else {
+            $validation_error['gender'] = "Enter a valid gender.";
+            return false;
+        }
+    } else {
+        $validation_error['gender'] = "gender is required.";
+        return false;
     }
-   
+}
+
 ?>
